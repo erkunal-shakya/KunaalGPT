e0fddb9a90b228ee62e0d5f9992187b10e5f2fe9
Kunal Shakya <92636603+erkunal-shakya@users.noreply.github.com>
2026-08-18 17:39:04 +0530

    Update KUNAAL_CONFIG with new API keys

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.KUNAAL_CONFIG = {
-  gemini: '',
-  groq: '',
-  openrouter: '',
-  huggingface: '',
-  mistral: '',
-  cloudflare: '',
-  cloudflareAccount: ''
+  gemini: 'AIzaSyDm8UI52Zx1givVb0GM2-292oYEOx5b-ZQ',
+  groq: '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',
+  openrouter: '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',
+  huggingface: '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',
+  mistral: 't0cumDpVJAQ3Qz43yFbENVgzotf68klm',
+  cloudflare: 'cfut_YaIvtE1LbSVnrs8WqeHLba6g8MUdgFok8E4L3NK7a9ef03ea',
+  cloudflareAccount: 'bf3f8ea6dd2bf3a7afa73985c8c248ee'
 };
